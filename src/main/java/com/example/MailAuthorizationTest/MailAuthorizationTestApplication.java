9f4eb2cbf12762a503db6538fc61728d8c069304
EugeniySichev <sichev91@inbox.ru>
2021-05-21 19:26:54 +0300

    initial commit

---
+++ src/main/java/com/example/MailAuthorizationTest/MailAuthorizationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.MailAuthorizationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAuthorizationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAuthorizationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MailAuthorizationTest/MailAuthorizationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MailAuthorizationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAuthorizationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
